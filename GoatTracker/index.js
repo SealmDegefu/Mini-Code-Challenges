ecc29849623bfe7242b98c8960b791611c12058b
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-12 16:50:18 -0700

    Completed assignment

--- GoatTracker/index.js
+++ GoatTracker/index.js
@@ -4,6 +4,38 @@ const toph = {
   brushesToday: 0,
   image:
     "https://st4.depositphotos.com/14435026/20983/i/600/depositphotos_209833190-stock-photo-single-black-white-tan-bearded.jpg",
-  badGoat: false,
+  badGoat: "BAD GOAT",
 };
-//  Code goes below here!
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  document.querySelector(".goat-name").innerText = toph.name
+  document.querySelector(".goat-age").innerText = toph.age
+  document.querySelector('.goat-image').innerText = toph.image
+  document.querySelector('.goat-status').innerText = toph.badGoat
+  document.querySelector('.goat-brushes').innerText= toph.brushesToday
+
+  document.querySelector('.brush-button').addEventListener ('click', () =>{
+  let count = document.querySelector('.goat-brushes').innerText
+  ++count 
+   document.querySelector('.goat-brushes').innerText = count
+    })
+  document.querySelector('.goat-toggle').addEventListener('click', () =>{
+    let toggleGoatValue = document.querySelector('.goat-status').innerText
+
+      toggleGoatValue === "BAD GOAT" ? toggleGoatValue = "Good goat": toggleGoatValue = "BAD GOAT"
+      document.querySelector('.goat-status').innerText = toggleGoatValue
+    })
+    // document.querySelector('.goat-toggle').addEventListener('click', () =>{
+    //   let changeWords = document.querySelector('#goat-redeemed').innerText
+
+    //   if (document.querySelector('.goat-status').innerText === true){
+    //     changeWords = "Have you been a good goat?"
+    //   }
+    //     else if (document.querySelector('.goat-status').innerText === false){
+    //           changeWords = "Have you been a bag goat?"
+    //     }
+    //     document.querySelector('#goat-redeemed').innerText = changeWords
+    // })
+})
+
